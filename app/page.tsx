96fe87be25bcf1912e10548b8f1b2ca9efca6f41
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2024-08-28 17:45:00 +0300

    feat: add base template of our layout and update global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
 	return (
 		<main>
 			<h1>Home</h1>
+			<Button variant='outline'>Cart</Button>
 		</main>
 	);
 }
