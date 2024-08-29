e48ec9fc7bcd6b4ac0a5a771789a969820ad892c
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2024-08-29 14:13:45 +0300

    feat: added sort component with top bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Container, Title } from '@/components/shared';
+import { Container, Title, TopBar } from '@/components/shared';
 
 export default function Home() {
 	return (
-		<Container className='mt-10'>
-			<Title text='Усі піци' size='lg' className='font-extrabold' />
-		</Container>
+		<>
+			<Container className='mt-10'>
+				<Title text='Усі піци' size='lg' className='font-extrabold' />
+			</Container>
+			<TopBar />
+		</>
 	);
 }
