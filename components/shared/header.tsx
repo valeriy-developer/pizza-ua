c42e017d27495a9eb5bc15f5e163c1d06892a2cd
Valeriy-Gnybidenko <valeriy.gnybidenko@gmail.com>
2024-08-29 09:21:06 +0300

    feat: created right side of header, added title component

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { cn } from '@/lib/utils';
-import { Container } from './container';
 import Image from 'next/image';
+import { ArrowRight, ShoppingCart, User } from 'lucide-react';
+import { Container } from './container';
+import { Button } from '../ui';
 
 interface Props {
 	className?: string;
@@ -11,11 +13,34 @@ export const Header: React.FC<Props> = ({ className }) => {
 	return (
 		<header className={cn('border border-b', className)}>
 			<Container className='flex items-center justify-between py-8'>
+				{/* Left side */}
 				<div className='flex items-center gap-4'>
 					<Image src='/logo.png' alt='Logo' width={35} height={35} />
 					<div>
 						<h1 className='text-2xl uppercase font-black'>Pizza UA</h1>
 						<p className='text-sm text-gray-400 leading-3'>Смачніше нікуди</p>
+					</div>
+				</div>
+				{/* Right side */}
+				<div className='flex items-center gap-3'>
+					<Button variant='outline' className='flex items-center gap-1'>
+						<User size={16} />
+						Увійти
+					</Button>
+
+					<div>
+						<Button className='group relative'>
+							<b>1000 грн.</b>
+							<span className='h-full w-[1px] bg-white/30 mx-3' />
+							<div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
+								<ShoppingCart size={16} className='relative' strokeWidth={2} />
+								<b>3</b>
+							</div>
+							<ArrowRight
+								size={20}
+								className='absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0'
+							/>
+						</Button>
 					</div>
 				</div>
 			</Container>
