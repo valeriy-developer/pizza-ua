7fe59c94b90446bb556eaabf8397f7d7fdd72a39
Valeriy-Gnybidenko <valeriy.gnybidenko@gmail.com>
2024-08-30 12:32:59 +0300

    feat: added filters, range-slider component and filter-checkbox on our home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Title, TopBar } from '@/components/shared';
+import { Container, Filters, Title, TopBar } from '@/components/shared';
 
 export default function Home() {
 	return (
@@ -7,6 +7,16 @@ export default function Home() {
 				<Title text='Усі піци' size='lg' className='font-extrabold' />
 			</Container>
 			<TopBar />
+			<Container className='mt-10 pb-14'>
+				<div className='flex gap-[60px]'>
+					<div className='w-[250px]'>
+						<Filters />
+					</div>
+					<div className='flex-1'>
+						<div className='flex flex-col gap-16'>Список товарів</div>
+					</div>
+				</div>
+			</Container>
 		</>
 	);
 }
--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -13,7 +13,6 @@ export const Header: React.FC<Props> = ({ className }) => {
 	return (
 		<header className={cn('border border-b', className)}>
 			<Container className='flex items-center justify-between py-8'>
-				{/* Left side */}
 				<div className='flex items-center gap-4'>
 					<Image src='/logo.png' alt='Logo' width={35} height={35} />
 					<div>
@@ -21,7 +20,6 @@ export const Header: React.FC<Props> = ({ className }) => {
 						<p className='text-sm text-gray-400 leading-3'>Смачніше нікуди</p>
 					</div>
 				</div>
-				{/* Right side */}
 				<div className='flex items-center gap-3'>
 					<Button variant='outline' className='flex items-center gap-1'>
 						<User size={16} />
